059e335728260a01f8ee28f0171cc09c88845203
toeysp130 <49556609+toeysp130@users.noreply.github.com>
2019-07-02 16:16:17 +0700

    Create w3hw.cpp

---
+++ w3hw.cpp
@@ -0,0 +1,13 @@
+﻿//โปรแกรมแปลงอุณหภูมิ
+#include<iostream>
+using namespace std;
+int main()
+{
+	float T=0,TF;
+	cout << "Enter Celsius temperature : " ;
+	cin >> T;
+	TF = T*1.8+32;
+	cout << "Fahrenheit = " << TF << endl;
+	cout << "Now weather in Thailand is " << ((TF < 70) ==0? "Hot":"Cool") << endl;
+	return(0);
+} 
